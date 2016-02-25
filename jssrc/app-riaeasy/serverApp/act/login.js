7e58ac7d9fd51281f20cf834e17ba6b7f9941dff
Administrator <riaeasy>
2016-02-25 21:01:29 +0800

    v1.0b

--- jssrc/app-riaeasy/serverApp/act/login.js
+++ jssrc/app-riaeasy/serverApp/act/login.js
@@ -26,32 +26,7 @@ define([
 				}
 			};
 
-		/*sql = "select distinct l.*, r.idp, r.cat, r.code, r.text, r.stat, r.typ, r.dcmd, r.ord, '1' as lvl, if(r.children=0, 'true', 'false') as leaf,\n"
-			+ "       if(length(r.id)<=3, 'true', 'false') as isroot, \n"
-			+ "       case r.expanded when '1' then 'true' when '0' then 'false' else null end as expanded,\n"
-			+ "       if(r.typ = '1', if(length(r.dicon)>1, r.dicon, if(r.children=0, 'applicationIcon', '')), 'acceptIcon') as dicon,\n"
-			+ "       if(length(r.diconfile)>1, r.diconfile, 'application.png') as diconfile\n"
-			+ "from (\n"
-			+ "       select r.didoper as idoper, r.didright as id\n"
-			+ "       from xoperright r\n"
-			+ "       where r.didoper='developer'\n"
-			+ "       union \n"
-			+ "       select p.account as idoper, l.idright as id\n"
-			+ "       from xoperrole p\n"
-			+ "       left join xroleright l\n"
-			+ "       on l.id=p.idrole\n"
-			+ "       where p.account='developer'\n"
-			+ "       union\n"
-			+ "       select 'developer' as idoper, t.id as id\n"
-			+ "       from xright t\n"
-			+ ") l\n"
-			+ "inner join xright r\n"
-			+ "on r.id=l.id\n"
-			+ "order by r.idp, r.ord";
-		rs = server.defaultDb.queryPage({
-			sql: sql
-		}, res);*/
-		sql = "select r.*\n"
+		/*sql = "select r.*\n"
 			+ "from xoper r\n"
 			+ "where r.code='" + code + "'\n";
 		rs = server.defaultDb.query1Array(sql);
@@ -68,7 +43,11 @@ define([
 			result.value.oper = {};
 			result.value.logged = false;
 			result.success = true;
-		}
+		}*/
+
+		result.success = true;
+		result.value.oper.code = code;
+		result.value.oper.name = code;
 
 		return result;
 
