c45522eb2dde624f1e92b42ae69b149eaeab9e6c
Administrator <riaeasy>
2016-07-06 17:45:50 +0800

    v1.0b4

--- runAll.bat
+++ runAll.bat
@@ -1,9 +1,10 @@
-﻿nginx -s stop
+
+nginx -s stop
 
 ping -n 2 127.0.0.1>nul
 
 start nginx
 
-rem 需要 java 8+
+rem needs java 8+
 
 start java -server -Xms512m -Xmx512m -XX:MetaspaceSize=16m -XX:MaxMetaspaceSize=256m -jar -Dfile.encoding=utf-8 RSServer.jar
