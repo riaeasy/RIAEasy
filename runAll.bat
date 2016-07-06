3600041d08a566bb4fff2ac40c56bdf6825ff63f
Administrator <riaeasy>
2016-07-06 14:50:15 +0800

    v1.0b4

--- runAll.bat
+++ runAll.bat
@@ -4,9 +4,6 @@ ping -n 2 127.0.0.1>nul
 
 start nginx
 
-
-
 rem 需要 java 8+
 
 start java -server -Xms512m -Xmx512m -XX:MetaspaceSize=16m -XX:MaxMetaspaceSize=256m -jar -Dfile.encoding=utf-8 RSServer.jar
-
--- runRiasServer.bat
+++ runRiasServer.bat
@@ -1,4 +1,3 @@
 ﻿rem 需要 java 8+
 
-
 start java -server -Xms512m -Xmx512m -XX:MetaspaceSize=16m -XX:MaxMetaspaceSize=256m -jar -Dfile.encoding=utf-8 RSServer.jar
